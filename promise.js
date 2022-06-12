1e1f5c2fc8b777bf942d89e7f48bea5c64e39e1d
Romeo-ag <romeo.a.gurning@gmail.com>
2022-06-12 09:22:41 -0400

    menambahkan definisi promise untuk menghitung jumlah film yang membuat marah dan tidak marah

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi)=>{
+  try{
+    const perasaan = (await promiseTheaterIXX()).concat(
+      await promiseTheaterVGC()
+    );
+    return perasaan.filter((funct)=> funct.hasil === emosi).length;
+  }catch(err){
+    console.error(err);
+  }
+
+};
 
 module.exports = {
   promiseOutput,
